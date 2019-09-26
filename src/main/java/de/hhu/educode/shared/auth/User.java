1f4d169c7527653a09f632661aec2f0f1ea389f1
Filip Krakowski <krakowski@hhu.de>
2019-09-26 13:17:42 +0200

    Implement reactive user holder using Reactor context

--- src/main/java/de/hhu/educode/shared/auth/User.java
+++ src/main/java/de/hhu/educode/shared/auth/User.java
@@ -14,4 +14,12 @@ public class User {
     private final String lastname;
     private final String email;
     private final Role role;
+
+    public boolean hasRole(Role role) {
+        return this.role.covers(role);
+    }
+
+    public boolean hasExactRole(Role role) {
+        return this.role == role;
+    }
 }
---
+++ src/main/java/de/hhu/educode/shared/reactor/ReactiveUserHolder.java
@@ -0,0 +1,66 @@
+package de.hhu.educode.shared.reactor;
+
+import de.hhu.educode.shared.auth.Role;
+import de.hhu.educode.shared.auth.User;
+import de.hhu.educode.shared.grpc.UserInterceptor;
+import io.grpc.Status;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
+
+import java.util.function.Function;
+
+public class ReactiveUserHolder {
+
+    private static final Class<?> USER_KEY = User.class;
+
+    public static Mono<User> currentUser() {
+        return Mono.subscriberContext()
+                .filter(ctx -> ctx.hasKey(USER_KEY))
+                .flatMap(ctx -> ctx.<Mono<User>>get(USER_KEY));
+    }
+
+    public static Function<Context, Context> clearContext() {
+        return context -> context.delete(USER_KEY);
+    }
+
+    public static Flux<?> withCurrentUser(Flux<?> flux) {
+        return flux.subscriberContext(Context.of(USER_KEY, UserInterceptor.CONTEXT_KEY.get()));
+    }
+
+    public static Mono<?> withCurrentUser(Mono<?> mono) {
+        return mono.subscriberContext(Context.of(USER_KEY, UserInterceptor.CONTEXT_KEY.get()));
+    }
+
+    public static <T> Mono<T> assertRole(Role role, Mono<T> mono) {
+        return mono.flatMap(it -> currentUser())
+                .switchIfEmpty(Mono.error(Status.UNAUTHENTICATED.asException()))
+                .filter(user -> user.hasRole(role))
+                .switchIfEmpty(Mono.error(Status.PERMISSION_DENIED.asException()))
+                .then(mono);
+    }
+
+    public static <T> Mono<T> assertExactRole(Role role, Mono<T> mono) {
+        return mono.flatMap(it -> currentUser())
+                .switchIfEmpty(Mono.error(Status.UNAUTHENTICATED.asException()))
+                .filter(user -> user.hasExactRole(role))
+                .switchIfEmpty(Mono.error(Status.PERMISSION_DENIED.asException()))
+                .then(mono);
+    }
+
+    public static <T> Flux<T> assertRole(Role role, Flux<T> flux) {
+        return flux.flatMap(it -> currentUser())
+                .switchIfEmpty(Mono.error(Status.UNAUTHENTICATED.asException()))
+                .filter(user -> user.hasRole(role))
+                .switchIfEmpty(Mono.error(Status.PERMISSION_DENIED.asException()))
+                .thenMany(flux);
+    }
+
+    public static <T> Flux<T> assertExactRole(Role role, Flux<T> flux) {
+        return flux.flatMap(it -> currentUser())
+                .switchIfEmpty(Mono.error(Status.UNAUTHENTICATED.asException()))
+                .filter(user -> user.hasExactRole(role))
+                .switchIfEmpty(Mono.error(Status.PERMISSION_DENIED.asException()))
+                .thenMany(flux);
+    }
+}
